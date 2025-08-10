31ee423b2ca95944280a7efb3940c2c9231904d4
Akash25tce <akash25aswinalr@gmail.com>
2025-08-10 19:27:11 +0530

    new update

--- src/data/events.js
+++ src/data/events.js
@@ -1,24 +1,27 @@
-// Export an array of event objects. Replace images with your files in /src/assets.
+import sih from '../assets/sih.jpg';
+import psg from '../assets/psg2024.jpg';
+import pals from '../assets/pals2025.jpg';
+
 export default [
   {
     id: 1,
     title: 'Smart India Hackathon 2023',
     date: '2023',
-    image: '../assets/sih.jpg',
+    image: sih,
     description: "Grand Finale organized by Ministry of Education & AICTE. Solved problem statement from Ministry of AYUSH."
   },
   {
     id: 2,
     title: 'PSG-Itech Hackfest Finals 2024',
     date: '2024',
-    image: '../assets/psg2024.jpg',
+    image: psg,
     description: 'Pitched disaster management mobile app and reached finals.'
   },
   {
     id: 3,
     title: "PALS Innowah Finals 2025",
     date: '2025',
-    image: '../assets/pals2025.jpg',
+    image: pals,
     description: 'Displayed VR based skill enhancement application.'
   }
-]
+];
